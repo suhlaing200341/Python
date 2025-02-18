aedbae7a01d63666ef0a3d86f943d7788b594c91
Su Hlaing <suhlaing200341@gmail.com>
2025-02-18 20:38:26 +0630

    delete .

--- string/tuple.py
+++
@@ -1,8 +0,0 @@
-fruits = ("apple", "orange", "papara")
-print("len of tuple.....", len(fruits))
-
-# if you want to use methods of list in tuple, the first to do is change tuple to list. and then, use methods of list.
-fruit_list = list(fruits)
-fruit_list.append("chocolate")
-fruits = tuple(fruit_list)
-print("fruit ....", fruits)
